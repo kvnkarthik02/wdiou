eedcee8519e00e52f4a1dc59f15c5c12b8987c67
Karthik Venkatesh <kvenkate@tcd.ie>
2025-06-22 18:16:57 +0400

    sign in confirmed, working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,16 +5,22 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+//user registration
 router.post('/register', async(req,res) =>{
     try{
-        const {name, email, password} = req.body;
+        //read request body
+        const {name, email, password} = req.body; 
+
+        //check if user email exists already
         const existingUser = await User.findOne({email});
         if (existingUser != null){
             return res.status(400).json({message: 'User already exists.'});
-            return;
         }
+        //new registration confirmed, calculate hash of password
         const passwordHash = await bcrypt.hash(password, 10);
+        //generate new user document
         const user = new User({name, email, passwordHash});
+        //save document
         await user.save();
         res.status(201).json({message:'User has been registered'})
     }
@@ -25,19 +31,22 @@ router.post('/register', async(req,res) =>{
 });
 
 router.get('/signin', async(req,res) => {
-    const user = {username, password}
-
     try{
+        //read request body
+        const {email, password} = req.body;
+
+        //check if user is registered already
         const existingUser = await User.findOne({email});
         if (existingUser) {
-            res.status(201).json({message:'User found.'})
-            const correctPass = bcrypt.compare(password, user.passwordHash)
+            // res.status(201).json({message:'User found.'})
+            // compare user provided password and password hash stored in db
+            const correctPass = await bcrypt.compare(password, existingUser.passwordHash);
             if (correctPass) {
-                res.status(201).json({message:'User found and password match.'})
-                const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, { expiresIn: '2d' });
-                res.json({jwtToken: token, user:{userId: user._id, name:user.name, email: user.email}});
+                //valid user sign-in, provide JWT token to user and perform successful sign-in
+                const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_TOKEN, { expiresIn: '2d' });
+                return res.status(201).json({jwtToken: token, user:{userId: existingUser._id, name:existingUser.name, email: existingUser.email}});
             }else {
-                res.status(400).json({message:'Incorrect password.'});
+                return res.status(400).json({message:'Incorrect password.'});
             }
         }else {
             res.status(400).json({message:'Invalid email, user not found.'});
