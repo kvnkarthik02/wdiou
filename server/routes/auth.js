0b3c9d2d5010623cf09f1fdcfb404d0c1cbf83a6
Karthik Venkatesh <kvenkate@tcd.ie>
2025-06-22 04:43:58 +0400

    user registration complete

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,21 +6,21 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req,res) =>{
-    const {name, email, password} = req.body;
-
     try{
+        const {name, email, password} = req.body;
         const existingUser = await User.findOne({email});
         if (existingUser != null){
             return res.status(400).json({message: 'User already exists.'});
+            return;
         }
-
-        const hash = await bcrypt.hash(password, 10);
+        const passwordHash = await bcrypt.hash(password, 10);
         const user = new User({name, email, passwordHash});
         await user.save();
         res.status(201).json({message:'User has been registered'})
     }
     catch(err){
-        res.status(500).json({message:'Server error.'})
+        res.status(500).json({message:err});
+        console.log(err);
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const auth = require('./routes/auth');
 require('dotenv').config();
 const app = express();
 app.use(cors());
-app.use(express.json());    
+// app.use(express.json());  
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))  
 app.use('/auth', auth);
 
 
@@ -12,7 +15,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
