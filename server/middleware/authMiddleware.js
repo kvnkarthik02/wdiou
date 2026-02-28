19344b7d5ad93945a2c885c5851245b2fbcce96d
Karthik Venkatesh <kvenkate@tcd.ie>
2026-02-28 01:25:52 +0000

    error code change

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,13 +4,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function auth(req,res, next){
     try {
+        //check if incoming header is available
         const header = req.headers.authorization || req.headers.Authorization
         if (!header) {
             return res.status(401).json({
                 message: 'Unauthorized, header unavailable.'
             });
         }
-
+            //if yes, split header into parts. else throw error
           const token = header && header.split(' ')[1];
           if (!token){
             return res.status(401).json({
@@ -18,13 +19,13 @@ module.exports = function auth(req,res, next){
             });
           }
           const payload = jwt.verify(token, process.env.JWT_TOKEN);
-          req.userID = payload.userID;
+          req.userId = payload.userId;
           req.user = payload;
           next();
     }catch(err){
-        console.error('Authentication Middleware Error (JWT error)', err);
-        return res.status(401).json({
-            message: 'Invalid/Expired JWT Token'
+        console.error('Authentication Middleware Error (JWT error).', err);
+        return res.status(403).json({
+            message: 'Invalid/Expired JWT Token.'
         });
     }
 };
